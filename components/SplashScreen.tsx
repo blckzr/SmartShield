448245a8d7725fffaf7295815d2aab4a174e3a89
Jan Kevin <jankevingerona15@gmail.com>
2025-06-29 21:12:01 +0800

    Updated Splash Screen

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -1,11 +1,26 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function SplashScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>🛡️SmartShield🛡️</Text>
-      <Text style={styles.sub}>Loading...</Text>
+      <Image
+        source={require("../assets/images/1_SmartShield - LOGO.png")}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      {/* Title: SmartShield */}
+      <Text style={styles.title}>
+        <Text style={styles.titleRed}>Smart</Text>
+        <Text style={styles.titleBlue}>Shield</Text>
+        <Text style={styles.titleColon}>:</Text>
+      </Text>
+
+      {/* Subtitle */}
+      <Text style={styles.subtitle}>
+        A Real-Time Heatwave Detection System{"\n"}With an Adaptive Shelter
+        Guidance
+      </Text>
     </View>
   );
 }
@@ -13,18 +28,32 @@ export default function SplashScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#233D91",
+    backgroundColor: "#fff",
     justifyContent: "center",
     alignItems: "center",
   },
+  logo: {
+    width: 200,
+    height: 200,
+  },
   title: {
-    color: "#fff",
-    fontSize: 28,
+    fontSize: 30,
     fontWeight: "bold",
   },
-  sub: {
-    color: "#fff",
-    fontSize: 16,
+  titleRed: {
+    color: "#FF3333", // Red for "Smart"
+  },
+  titleBlue: {
+    color: "#233D91", // Blue for "Shield"
+  },
+  titleColon: {
+    color: "#233D91",
+    fontWeight: "bold",
+  },
+  subtitle: {
+    color: "#233D91",
+    fontSize: 14,
+    textAlign: "center",
     marginTop: 10,
   },
 });
